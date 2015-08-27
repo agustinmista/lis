bc5b08a405d24f129bc1adcce0a3dd5feba15ce3
Agustín Mista <mista.agustin@gmail.com>
2015-08-26 21:44:52 -0300

    lookfor, update and half evalComm

--- Eval1.hs
+++ Eval1.hs
@@ -3,21 +3,27 @@ module Eval1 (eval) where
 import AST
 
 -- Estados
-type State = [(Variable,Int)]
+type State = [(Variable, Int)]
+
 
 -- Estado nulo
 initState :: State
 initState = []
 
 -- Busca el valor de una variabl en un estado
--- Completar la definicion
 lookfor :: Variable -> State -> Int
-lookfor = undefined
+lookfor v [] = error "Var not in scope"
+lookfor v (x:xs) = if v == fst x
+                   then snd x
+                   else lookfor v xs
 
 -- Cambia el valor de una variable en un estado
--- Completar la definicion
+-- (Si la variable no existe la agrega al final) CONSULTAR!
 update :: Variable -> Int -> State -> State
-update = undefined
+update v i [] = [(v,i)]
+update v i (x:xs) = if v == fst x
+                    then (v,i) : xs
+                    else x : (update v i xs)
 
 -- Evalua un programa en el estado nulo
 eval :: Comm -> State
@@ -26,7 +32,12 @@ eval p = evalComm p initState
 -- Evalua un comando en un estado dado
 -- Completar definicion
 evalComm :: Comm -> State -> State
-evalComm = undefined
+evalComm c state = case c 
+                   of Skip                  -> error "Skip!"
+                      Let var iexp          -> error "Let!"
+                      Seq comm1 comm2       -> error "Seq!"
+                      Cond cond commT commF -> error "Cond!"
+                      While cond comm       -> error "While!"
 
 -- Evalua una expresion entera, sin efectos laterales
 -- Completar definicion
